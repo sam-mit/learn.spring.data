1e32951a7dd68df8b46313eff2bac1ef00214d7e
soumits <soumit.saha@vodafone.com>
2020-10-13 23:19:13 +0100

    ... basic book repo

--- src/main/java/com/springdata/data/entities/Book.java
+++ src/main/java/com/springdata/data/entities/Book.java
@@ -7,9 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy;
 
 @Entity
 @Table(name = "BOOK")
